03d40cbac4b53ee507f8b0c715bd92e4a52d9ffc
Iuliana Lapatkina <dolganova.yuliana@yandex.ru>
2023-05-05 15:04:55 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,39 @@
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+  }
+});
+
+let buttonShow = document.querySelector('.button-show');
+let buttonHide = document.querySelector('.button-hide');
+
+buttonShow.addEventListener("click", showElem);
+buttonHide.addEventListener("click", hideElem);
+		
+function showElem() {
+	let servicesItem = document.querySelectorAll('.services-item');
+  for (i=0; i<servicesItem.length; i++) {
+    servicesItem[i].style.display = 'block';
+  }
+	buttonShow.style.display = 'none';
+  buttonHide.style.display = 'block';
+}
+
+function hideElem() {
+	let servicesItemHideTablet = document.querySelectorAll('.services-item:nth-child(n+7)');
+  let servicesItemHideDesktop = document.querySelectorAll('.services-item:nth-child(n+9)');
+  if ((document.documentElement.clientWidth >= 768) &&
+  (document.documentElement.clientWidth < 1120)) {
+    for (i=0; i<servicesItemHideTablet.length; i++) {
+      servicesItemHideTablet[i].style.display = 'none';
+    }
+  }
+  if (document.documentElement.clientWidth > 1120) {
+    for (i=0; i<servicesItemHideDesktop.length; i++) {
+      servicesItemHideDesktop[i].style.display = 'none';
+    }
+  }
+	buttonShow.style.display = 'block';
+  buttonHide.style.display = 'none';
+}
